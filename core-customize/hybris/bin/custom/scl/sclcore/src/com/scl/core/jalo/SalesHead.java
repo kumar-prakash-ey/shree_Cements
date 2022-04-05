b294386fa307a180cb9a08553773b8252d805d43
Prakash <prakash.kumar5@in.ey.com>
2022-04-05 17:18:15 +0530

    Scl users model

--- core-customize/hybris/bin/custom/scl/sclcore/src/com/scl/core/jalo/SalesHead.java
+++ core-customize/hybris/bin/custom/scl/sclcore/src/com/scl/core/jalo/SalesHead.java
@@ -6,10 +6,10 @@ import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.type.ComposedType;
 import org.apache.log4j.Logger;
 
-public class SclDealer extends GeneratedSclDealer
+public class SalesHead extends GeneratedSalesHead
 {
 	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger( SclDealer.class.getName() );
+	private static final Logger LOG = Logger.getLogger( SalesHead.class.getName() );
 	
 	@Override
 	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
